65b265307a8b84e9a8364bb09ea22b19443a6c1c
Lankenau <dlankena@chicagobooth.edu>
2015-05-20 20:25:04 -0500

    Complete in class work

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+  validates :title, :presence => true
+  validates :director, :presence => true
+
+  belongs_to :director
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,7 @@
 class Role < ActiveRecord::Base
+  validates :actor, :presence => true
+  validates :movie, :presence => true
+
+  belongs_to :actor
+  belongs_to :movie
 end
